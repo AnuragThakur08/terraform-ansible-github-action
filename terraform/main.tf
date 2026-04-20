aa80b82d4e30bb0cfb51cd3da30022a3b7ba9351
Anurag Singh <Anurag@abhisheks-MacBook-Air.local>
2026-04-21 01:59:23 +0530

    Expose ports and install Prometheus + Grafana

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,23 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+# Grafana (UI)
+ingress {
+  description = "Grafana"
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
+# Prometheus
+ingress {
+  description = "Prometheus"
+  from_port   = 9090
+  to_port     = 9090
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
   ingress {
     description = "HTTP"
     from_port   = 80
